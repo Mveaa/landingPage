a02818c762d16c4bfae17720fb81245d40b08256
Kacper Olszewski <kacper.olszewski@psp-media.com>
2022-03-02 21:29:39 +0100

    add carousel

--- src/app/pages/home/components/carousel.component.ts
+++ src/app/pages/home/components/carousel.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+const enum CarouselSize {
+  LARGE = 4,
+  MEDIUM = 3,
+  SMALL = 2
+}
 
 @Component({
   selector: 'app-home-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('carousel')
+  public carousel: ElementRef;
+  private container: HTMLDivElement;
+  private slides: number;
+  private interval: number;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.container = this.carousel.nativeElement as HTMLDivElement;
+    this.slides = this.container.children.length;
+
+    let i = 0;
+    const limit = this.slides - CarouselSize.LARGE;
+
+    this.interval = setInterval(() => {
+      if (i < limit) {
+        i++;
+      } else {
+        i = 0;
+      }
+
+      this.container.style.transform = `translateX(${i * -25}%)`;
+    }, 5000);
   }
 
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
